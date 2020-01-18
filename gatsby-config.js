3a3e2b42967bab70cb0b6a742a85c94ea2c2a69d
Ikram Kharbouch <ikrkharb@e1r6p11.1337.ma>
2020-01-19 00:48:36 +0100

    Add /blog and Markdown support

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,15 @@
  */
 
 module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`],
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/blog`,
+        name: `markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`
+  ]
 }
